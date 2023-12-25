3d2a8e763b3fa13aa9473bc5f534f29dbe8ba3e6
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2023-12-25 19:38:33 +0100

    feat(errorPage): Adding error page
    Adding error page style (WIP)
    Fixing components style

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,10 +1,17 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { NavLink } from "react-router-dom";
 
 function Error404() {
     return (
         <div>
             <Header />
-            <h1>Erreur 404</h1>
+            <div className="error">
+                <h1 className="error-title">404</h1>
+                <h2 className="error-msg">Oups! La page que vous demandez n'existe pas.</h2>
+                <NavLink to="/" className="error-link">Retourner sur la page d'accueil</NavLink>
+            </div>
+            <Footer />
         </div> 
     )
 }
