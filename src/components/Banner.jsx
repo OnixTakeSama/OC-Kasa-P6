db337468597c99c9577a0ba74ad5a07c01f59a21
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2024-01-05 16:05:39 +0100

    feat(LodgingPage): Adding lodging pages

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,16 @@
 import BannerImg from '../images/banner.png';
+import { useParams } from 'react-router-dom';
+import lodgings from '../lodgings.json';
 
 function Banner() {
+
+  const { id } = useParams();
+  const lodging = lodgings.find((lodging) => lodging.id === id);
+
+  if (!lodging) {
+    return <div>Lodging not found</div>;
+  }
+
   return (
     <section className="banner">
         <h1>Chez vous, partout et ailleurs</h1>
