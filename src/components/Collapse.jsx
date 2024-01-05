ced482fea0cacbac2c70de6010a26ef578f6780c
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2024-01-05 14:02:31 +0100

    fix(CSS): Fixing CSS
    Fixed CSS animation
    Renamed pages

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ChevronUp from '../images/chevron-up.png';
+
+const Collapse = ({ title, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className={`collapse ${isOpen ? 'open' : ''}`}>
+            <button onClick={toggleCollapse}>
+                <h1 className='title'>{title}</h1>
+                <span className={`chevron ${isOpen ? 'rotate' : ''}`}><img src={ChevronUp} alt="Chevron"></img></span>
+            </button>
+            <div className={isOpen ? 'content-parent show' : 'content-parent'}>
+                <div className='content'>
+                    <p>{content}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Collapse;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import Dropdown from "../components/Dropdown";
+import Collapse from "../components/Collapse";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import BannerImg from '../images/about.webp';
@@ -10,15 +10,18 @@ function About() {
             <section className="banner">
                 <img src={BannerImg} alt="Paysage de falaises" className="banner-img"/>
             </section>
-            <section className="dropdown-list">
-                <Dropdown title="Fiabilité" dropdownContent="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            <section className="collapse-list">
+                <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les
                 photos sont conformes aux logements, et toutes les informations sont
                 régulièrement vérifiées par nos équipes."/>
-                <Dropdown title="Respect" dropdownContent="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
+
+                <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
                 discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
-                <Dropdown title="Service" dropdownContent="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
+
+                <Collapse title="Service" content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
                 N'hésitez pas à nous contacter si vous avez la moindre question."/>
-                <Dropdown title="Sécurité" dropdownContent="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les
+
+                <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les
                 voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. Enfin, notre plateforme vous permet de discuter avec votre 
                 hôte avant votre arrivée afin de vous assurer que le logement correspond bien à vos attentes."/>
             </section>
