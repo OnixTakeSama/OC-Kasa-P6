44ba8b4bab26f3230905e45f601a0030b2d3e97f
Benjamin W <71712365+OnixTakeSama@users.noreply.github.com>
2024-01-19 13:40:41 +0100

    fix(CSS): Fixing CSS
    Last fixes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,6 @@
 import BannerImg from '../images/banner.png';
-import { useParams } from 'react-router-dom';
-import lodgings from '../lodgings.json';
 
 function Banner() {
-
-  const { id } = useParams();
-  const lodging = lodgings.find((lodging) => lodging.id === id);
 
   return (
     <section className="banner">
